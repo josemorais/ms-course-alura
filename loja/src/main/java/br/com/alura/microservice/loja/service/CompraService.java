db72bb0230ebbe9bc2d3d3c2da432c92902c9c03
Jose Junior <jjunior@sccalpha.local>
2021-11-05 12:26:50 -0300

    Configurando o eureka server e client

--- loja/src/main/java/br/com/alura/microservice/loja/service/CompraService.java
+++ loja/src/main/java/br/com/alura/microservice/loja/service/CompraService.java
@@ -1,5 +1,6 @@
 package br.com.alura.microservice.loja.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -10,10 +11,12 @@ import br.com.alura.microservice.loja.controller.dto.InfoForncedorDTO;
 
 @Service
 public class CompraService {
+	
+	@Autowired
+	private RestTemplate restTemplate;
 
 	public void realizaCompra(CompraDTO compra) {
-		RestTemplate restTemplate = new RestTemplate();
-		ResponseEntity<InfoForncedorDTO> exchange = restTemplate.exchange("http://localhost:8081/info/"+compra.getEndereco().getEstado(), HttpMethod.GET, null, InfoForncedorDTO.class);
+		ResponseEntity<InfoForncedorDTO> exchange = restTemplate.exchange("http://fornecedor/info/"+compra.getEndereco().getEstado(), HttpMethod.GET, null, InfoForncedorDTO.class);
 		
 		System.out.println(exchange.getBody().getEndereco());
 	}
