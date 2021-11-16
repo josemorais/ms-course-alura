b5e9bd1bbb00be4c465fd5e02437b801f64f0d84
Jose Junior <jjunior@sccalpha.local>
2021-11-16 10:04:43 -0300

    Criação banco de dados pra loja e usando bulkhead (disponibilização de threads para cadas método)

--- loja/src/main/java/br/com/alura/microservice/loja/service/CompraService.java
+++ loja/src/main/java/br/com/alura/microservice/loja/service/CompraService.java
@@ -1,32 +1,28 @@
 package br.com.alura.microservice.loja.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
-import br.com.alura.microservice.loja.client.FornecedorClient;
-import br.com.alura.microservice.loja.controller.dto.CompraDTO;
-import br.com.alura.microservice.loja.controller.dto.InfoForncedorDTO;
-import br.com.alura.microservice.loja.controller.dto.InfoPedidoDTO;
-import br.com.alura.microservice.loja.model.Compra;
+import br.com.alura.microservice.loja.dto.CompraDTO;
+import br.com.alura.microservice.loja.dto.InfoFornecedorDTO;
 
 @Service
 public class CompraService {
 	
 	@Autowired
-	private FornecedorClient fornecedorClient;
+	private RestTemplate client;
 
-	public Compra realizaCompra(CompraDTO compra) {
-		InfoForncedorDTO info = fornecedorClient.getInfoPorEstado(compra.getEndereco().getEstado());
+	public void realizaCompra(CompraDTO compra) {
 		
-		InfoPedidoDTO pedido = fornecedorClient.realizaPedido(compra.getItens());
-		System.out.println(info.getEndereco());
+		ResponseEntity<InfoFornecedorDTO> exchange = 
+			client.exchange("http://fornecedor/info/"+compra.getEndereco().getEstado(),
+			HttpMethod.GET, null, InfoFornecedorDTO.class);
 		
-		Compra compraSalva = new Compra();
-		compraSalva.setPedidoId(pedido.getId());
-		compraSalva.setTempoDePreparo(pedido.getTempoDePreparo());
-		compraSalva.setEndereco(compra.getEndereco().toString());
+		System.out.println(exchange.getBody().getEndereco());
 		
-		return compraSalva;
 	}
 
 }
--- loja/src/test/java/br/com/alura/microservice/loja/LojaApplicationTests.java
+++ loja/src/test/java/br/com/alura/microservice/loja/LojaApplicationTests.java
@@ -1,13 +1,16 @@
 package br.com.alura.microservice.loja;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class LojaApplicationTests {
+public class LojaApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
 	}
 
 }
